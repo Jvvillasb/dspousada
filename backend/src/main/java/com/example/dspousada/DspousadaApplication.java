75824d611165cde7046a830e67a141fd007aed9e
João Vitor Villas Bôas <jvvillasb@gmail.com>
2022-08-09 23:44:17 -0300

    Projeto Backend Criado

---
+++ backend/src/main/java/com/example/dspousada/DspousadaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dspousada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspousadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspousadaApplication.class, args);
+	}
+
+}
